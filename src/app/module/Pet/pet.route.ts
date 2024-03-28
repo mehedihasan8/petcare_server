c06e752137454cd706ed950e472f0eeb92c2c13e
jahid <jahidmorol2@gmail.com>
2024-03-28 13:18:54 +0600

    implant auth funtionalities

--- src/app/module/Pet/pet.route.ts
+++ src/app/module/Pet/pet.route.ts
@@ -2,11 +2,14 @@ import express from "express";
 import validationRequest from "../../middlewares/validationRequest";
 import { PetValidation } from "./pet.validation";
 import { PetController } from "./pet.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
 router.post(
   "/",
+  auth(UserRole.USER),
   validationRequest(PetValidation.createPetValidation),
   PetController.createPet
 );
