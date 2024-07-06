238a6b87843210402ef7d0f148e9820ff3033145
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2024-07-06 12:33:44 +0600

    add my url

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const main = async () => {
   const server: Server = app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+    console.log(`App runnige on port ${port}`);
   });
 };
 
